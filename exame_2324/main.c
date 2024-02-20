0bd9fe386f844c1e9d65e00ae5ece079b9d51c1e
heyliceeee <58340504+heyliceeee@users.noreply.github.com>
2024-02-20 10:30:50 +0000

    :sparkles: feat: exame 2023/2024 ex2

--- exame_2324/main.c
+++ exame_2324/main.c
@@ -2,7 +2,7 @@
 
 /**
  * conta os espacos em branco presentes no array char
- * @param s string
+ * @param s array
  * @return a soma dos espacos em branco presentes no array
  */
 int contaEspacosEmBranco(char *s)
@@ -20,10 +20,36 @@ int contaEspacosEmBranco(char *s)
     return sum;
 }
 
+
+/**
+ * devolve a ultima posicao em que uma letra ocorre no array
+ * @param s array
+ * @param c letra
+ * @return devolve a ultima posicao em que uma letra ocorre no array, caso contrario 0
+ */
+int devolveUltimaPosicao(char *s, char c)
+{
+    int lastPos=0;
+
+    for(int i=0; s[i] != '\0'; i++)
+    {
+        if(s[i] == c)
+        {
+            lastPos = i+1;
+        }
+    }
+
+    return lastPos;
+}
+
+
+
+
 int main() {
     printf("START\n\n");
 
     printf("soma dos espacos em branco: %d \n", contaEspacosEmBranco("hello world"));
+    printf("ultima posicao de l (0 - nao existe): %d \n", devolveUltimaPosicao("hello world", 'l'));
 
     printf("\nEND\n\n");
     return 0;
