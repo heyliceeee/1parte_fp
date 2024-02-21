13f39436b87123064ca39bf18e9148c4942ed288
heyliceeee <58340504+heyliceeee@users.noreply.github.com>
2024-02-21 09:19:50 +0000

    :sparkles: feat: exame modelo1 ex1

--- exame_modelo1/main.c
+++ exame_modelo1/main.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+#define NUM_CIDADES 6
+#define TAM_TRAJETO 6
+
+
+
+/**
+ * dado um trajeto entre as cidades, verifica se o trajeto e possivel
+ * @param trajeto array
+ * @param cidades matriz
+ * @return 1 se o trajeto for possivel, caso contrario 0
+ */
+int verificaTrajeto(int trajeto[TAM_TRAJETO], int cidades[NUM_CIDADES][NUM_CIDADES])
+{
+    for(int i=0; i < TAM_TRAJETO-1; i++) //percorrer trajetos
+    {
+        if(cidades[trajeto[i]][trajeto[i+1]] == 0) //verificar se na cidade atual e da seguinte ha conexao
+        {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+
+
+int main()
+{
+    int cidades[NUM_CIDADES][NUM_CIDADES] = {
+            {1, 0, 1, 0, 0, 1},
+            {0, 1, 1, 1, 1, 1},
+            {1, 1, 1, 0, 1, 1},
+            {0, 1, 0, 1, 1, 0},
+            {0, 0, 1, 1, 1, 1},
+            {1, 1, 1, 0, 1, 1}
+    };
+    int trajeto[TAM_TRAJETO] = {0, 2, 1, 3, 4, 5};
+
+
+    printf("START\n");
+
+    printf("trajeto possivel: %d\n", verificaTrajeto(trajeto, cidades));
+
+    printf("\nEND\n\n");
     return 0;
 }
